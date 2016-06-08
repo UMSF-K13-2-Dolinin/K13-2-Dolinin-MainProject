76323cacc325372be786123c347b865ffa4f81f9
UMSF-K13-2-Dolinin <dremor1995@yandex.ru>
2016-06-08 12:09:09 +0300

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bs = new BaseClass();
+            bs.Method();
+            bs = new SomeCLass();
+            bs.Method();
+            Console.ReadLine();
+        }
+    }
+    class BaseClass
+    {
+        public virtual void Method()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+    class SomeCLass : BaseClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Hello Gleb");
+        }
+    }
+}
